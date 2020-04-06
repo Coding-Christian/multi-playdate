ab5f9ba94d97547e523bfe5782e2435c040ae5ae
Coding-Christian <christianhaney1996@gmail.com>
2020-04-06 12:46:41 -0700

    added font added focus and active styles to button for accessibility

--- src/components/styledButton.jsx
+++ src/components/styledButton.jsx
@@ -4,11 +4,21 @@ import styled from '@emotion/styled';
 function StyledButton(props) {
   const StyledButton = styled.button`
     height: 50px;
+    font-family: "Raleway", sans-serif;
+    font-size: 1.5rem;
     background-color: #fff;
     padding: 0px 20px;
     border: none;
     border-radius: 12px;
     box-shadow: 5px 5px 10px #d9d9d9, -5px -5px 10px #ffffff;
+    &:focus {
+      text-decoration: underline;
+      outline: none;
+    }
+    &:hover {
+      background: linear-gradient(145deg, #e6e6e6, #ffffff);
+      box-shadow: 5px 5px 12px #d9d9d9, -5px -5px 12px #ffffff;
+    }
     &:active {
       box-shadow: inset 5px 5px 10px #e0e0e0, inset -5px -5px 10px #ffffff;
     }
