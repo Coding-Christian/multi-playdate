20a759f72da063189c19809bbd30a4e8fe48dca9
Jacob Mikesell <jacob.mikesell96@gmail.com>
2020-04-08 15:43:27 -0700

    first pass at first endpoint

--- server/main.go
+++ server/main.go
@@ -1,20 +1,165 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	// "./models/steamGameList"
+
+	"github.com/gorilla/mux"
 )
 
 type server struct{}
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "hello world"}`))
+func getGamesForPlayer(ID string) ([]Game, error) {
+	steamAllGamesURL := fmt.Sprintf("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json", apiKey, ID)
+	resp, err := http.Get(steamAllGamesURL)
+	if err != nil {
+		return nil, errors.New("Error with Request")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("Error reading body Request")
+	}
+	steamgames, err := UnmarshalSteamGameListResponse(body)
+	if err != nil {
+		return nil, errors.New("Error parsing Request")
+	}
+
+	return steamgames.GameList.Games, nil
 }
 
+func convertGameSliceToSet(games []Game) Set {
+	gameSet := NewSet()
+	for _, elem := range games {
+		gameSet.m[elem.Appid] = true
+	}
+	return *gameSet
+}
+
+func findCommonGames(allGames [][]Game) []int {
+	primarySet := convertGameSliceToSet(allGames[0])
+	for _, gameSlice := range allGames[1:] {
+		primarySet.Union(convertGameSliceToSet(gameSlice))
+	}
+	sharedGames := make([]int, 0, len(primarySet.m))
+	for k := range primarySet.m {
+		sharedGames = append(sharedGames, k)
+	}
+
+	return sharedGames
+}
+
+func getDetailsForGame(appID int) (*GameInfo, error) {
+	steamGameInfoURL := fmt.Sprintf("https://store.steampowered.com/api/appdetails?appids=%d", appID)
+	fmt.Println(steamGameInfoURL)
+	resp, err := http.Get(steamGameInfoURL)
+	if err != nil {
+		fmt.Println(err)
+
+		return nil, errors.New("Error with Request")
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New("Error reading body Request")
+	}
+	gameInfo, err := UnmarshalSteamGameInfoResponse(body)
+	if err != nil {
+		return nil, errors.New("Error parsing Request")
+	}
+	// fmt.Println(gameInfo)
+	var response GameInfo
+	for _, v := range gameInfo {
+		response = v.GameInfo
+	}
+	return &response, nil
+}
+
+func getDetailsForGames(appIDs []int) ([]GameInfo, error) {
+	allGameInfo := make([]GameInfo, 0, len(appIDs))
+	for i := range appIDs {
+		gameInfo, err := getDetailsForGame(appIDs[i])
+		if err != nil {
+			fmt.Println("error with %d : %s", appIDs[i], err)
+			continue
+		}
+		allGameInfo = append(allGameInfo, *gameInfo)
+	}
+	return allGameInfo, nil
+}
+
+func getGamesForAllPlayers(IDs []string) ([]GameInfo, error) {
+	allGames := make([][]Game, len(IDs))
+
+	for i, ID := range IDs {
+		games, err := getGamesForPlayer(ID)
+		if err != nil {
+			return nil, err
+		}
+		allGames[i] = games
+	}
+	sharedGames := findCommonGames(allGames)
+
+	gameInfo, err := getDetailsForGames(sharedGames)
+	if err != nil {
+		return nil, errors.New("Error parsing Request")
+	}
+	return gameInfo, nil
+}
+
+func filterByMultiplayer(games []GameInfo) []GameInfo {
+	acceptableTypes := map[string]bool{"Multi-player": true, "Co-op": true, "Local Multi-Player": true, "Local Co-op": true, "MMO": true}
+	filteredGames := make([]GameInfo, 0, len(games))
+	for i := range games {
+		types := games[i].Categories
+		for _, elem := range types {
+			if _, ok := acceptableTypes[*elem.Description]; ok {
+				filteredGames = append(filteredGames, games[i])
+				break
+			}
+		}
+	}
+	return filteredGames
+}
+
+func sharedGames(w http.ResponseWriter, r *http.Request) {
+	var steamIDs []string
+
+	err := json.NewDecoder(r.Body).Decode(&steamIDs)
+	// steamIDs := []string{"76561198076015385", "76561197991893905", "76561198057020032"}
+	if err != nil {
+		w.WriteHeader(http.StatusNotAcceptable)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	allGames, err := getGamesForAllPlayers(steamIDs)
+	multiplayerGames := filterByMultiplayer(allGames)
+	if err != nil {
+		log.Fatalln("There was a problem")
+	}
+
+	bytes, _ := json.Marshal(multiplayerGames)
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(bytes))
+}
+
+const apiKey = "72447D2AB792726DA3F6EB87F871A943"
+
+//
 func main() {
-	s := &server{}
-	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	router := mux.NewRouter()
+
+	api := router.PathPrefix("/api").Subrouter()
+	api.HandleFunc("/shared/games", sharedGames).Methods(http.MethodPost)
+	log.Fatal(http.ListenAndServe(":8000", router))
 }
---
+++ server/steamGameList.go
@@ -0,0 +1,101 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse and unparse this JSON data, add this code to your project and do:
+//
+//    steamGameListResponse, err := UnmarshalSteamGameListResponse(bytes)
+//    bytes, err = steamGameListResponse.Marshal()
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+func UnmarshalSteamGameListResponse(data []byte) (SteamGameListResponse, error) {
+	var r SteamGameListResponse
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *SteamGameListResponse) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func (r *Response) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+func (r *Game) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type SteamGameListResponse struct {
+	GameList GameList `json:"response"`
+}
+
+type GameList struct {
+	GameCount int    `json:"game_count"`
+	Games     []Game `json:"games"`
+}
+
+type Game struct {
+	Appid           int  `json:"appid"`
+	PlaytimeForever int  `json:"playtime_forever"`
+	Playtime2Weeks  *int `json:"playtime_2weeks,omitempty"`
+}
+
+func UnmarshalSteamGameInfoResponse(data []byte) (SteamGameInfoResponse, error) {
+	var r SteamGameInfoResponse
+	err := json.Unmarshal(data, &r)
+	if err != nil {
+		fmt.Println(err)
+
+	}
+	return r, err
+}
+
+func (r *SteamGameInfoResponse) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type SteamGameInfoResponse map[string]Response
+
+type Response struct {
+	Success  bool     `json:"success"`
+	GameInfo GameInfo `json:"data"`
+}
+
+type GameInfo struct {
+	Type             *string      `json:"type,omitempty"`
+	Name             *string      `json:"name,omitempty"`
+	SteamAppid       *int64       `json:"steam_appid,omitempty"`
+	IsFree           *bool        `json:"is_free,omitempty"`
+	ShortDescription *string      `json:"short_description,omitempty"`
+	HeaderImage      *string      `json:"header_image,omitempty"`
+	Metacritic       *Metacritic  `json:"metacritic,omitempty"`
+	Categories       []Category   `json:"categories"`
+	Genres           []Genre      `json:"genres"`
+	Screenshots      []Screenshot `json:"screenshots"`
+	Background       *string      `json:"background,omitempty"`
+}
+
+type Category struct {
+	ID          *int64  `json:"id,omitempty"`
+	Description *string `json:"description,omitempty"`
+}
+
+type Genre struct {
+	ID          *string `json:"id,omitempty"`
+	Description *string `json:"description,omitempty"`
+}
+
+type Metacritic struct {
+	Score *int64  `json:"score,omitempty"`
+	URL   *string `json:"url,omitempty"`
+}
+
+type Screenshot struct {
+	ID            *int64  `json:"id,omitempty"`
+	PathThumbnail *string `json:"path_thumbnail,omitempty"`
+	PathFull      *string `json:"path_full,omitempty"`
+}
