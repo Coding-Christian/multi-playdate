eac4b22eab3d93b002f50aa64278475eaaeea022
Coding-Christian <christianhaney1996@gmail.com>
2020-04-08 13:59:57 -0700

    styled user entry component

--- src/components/userEntry.jsx
+++ src/components/userEntry.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-// import styled from '@emotion/styled';
+import styled from '@emotion/styled';
 import StyledH1 from './styledH1';
 import StyledButton from './styledButton';
 import StyledInput from './styledInput';
 
 function UserEntry(props) {
   // const [steamId, setSteamId] = useState('');
+  const StyledForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: fit-content;
+    background: #f7f7f7;
+    padding: 40px;
+    border-radius: 5%;
+    box-shadow: -5px -5px 7px #fff, 5px 5px 7px rgba(94, 104, 121, 0.712);
+    position: relative;
+    &:after {
+      content: "";
+      position: absolute;
+      height: calc(100% - 20px);
+      width: calc(100% - 20px);
+      background-color: transparent;
+      border-radius: 5%;
+      box-shadow: inset -5px -5px 7px #ffff,
+        inset 5px 5px 7px rgba(94, 104, 121, 0.671);
+    }
+  `;
   return (
-    <>
+    <StyledForm>
       <StyledH1>MultiPlayDate</StyledH1>
-      <StyledInput type="number" maxLength="17" required />
-      <StyledButton type="button">Play</StyledButton>
-    </>
+      <div>
+        <StyledInput
+          placeholder="SteamID"
+          type="number"
+          maxLength="17"
+          required
+        />
+        <StyledButton type="button">Play</StyledButton>
+      </div>
+    </StyledForm>
   );
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StyledH1 from './components/styledH1';
-import StyledButton from './components/styledButton';
-import StyledInput from './components/styledInput';
+import UserEntry from './components/userEntry';
 
-ReactDOM.render(
-  <>
-    <StyledH1>Hello World!</StyledH1>
-    <StyledButton title="Click Me!">Click Me!</StyledButton>
-    <StyledInput type="text" placeholder="Type Here!" />
-  </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<UserEntry />, document.getElementById('root'));
