c27e65147afedbba13dc50b4f3609692788d5142
Jacob Mikesell <jacob.mikesell96@gmail.com>
2020-04-21 20:06:49 -0700

    added new endpoint and refactored steam api a little

--- server/set.go
+++ server/set.go
@@ -1,17 +1,16 @@
 package main
 
+//Set is a simple set implimentation
 type Set struct{ m map[int]bool }
 
+//NewSet Returns an empty *set
 func NewSet() *Set {
 	s := &Set{}
 	s.m = make(map[int]bool)
 	return s
 }
 
-type Unioner interface {
-	Union(otherSet map[int]bool)
-}
-
+//Union removes keys from set s that are not in otherset
 func (s *Set) Union(otherSet Set) {
 	for key := range s.m {
 		if _, ok := otherSet.m[key]; !ok {
